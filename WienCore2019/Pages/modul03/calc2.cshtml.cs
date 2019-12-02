27ba40e1a50f24a560fd4b5fde249af077fd870c
bill gates <hannesp@ppedv.de>
2019-12-02 15:48:28 +0100

    Razor Page Handler Calc

---
+++ WienCore2019/Pages/modul03/calc2.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+
+    public class calc2Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPostPlus([FromForm] int eins,[FromForm] int zwei)
+        {
+            Ergebnis = eins + zwei;
+        }
+        public void OnPostMinus([FromForm] int eins, [FromForm] int zwei)
+        {
+            Ergebnis = eins -zwei;
+        }
+
+    }
+}
