54dd7a52dc1da01358bc77521fb11aa25131e162
bill gates <hannesp@ppedv.de>
2019-12-02 16:00:31 +0100

    Razor Databinding

---
+++ WienCore2019/Pages/modul03/calc3.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class calc3Model : PageModel
+    {
+        public int Ergebnis { get; set; }
+        public int eins { get; set; }
+        public int zwei { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public void OnPostPlus()
+        {
+            Ergebnis = eins + zwei;
+        }
+        public void OnPostMinus()
+        {
+            Ergebnis = eins - zwei;
+        }
+    }
+}
