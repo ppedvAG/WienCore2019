aff5d3a818dc2d4f18f8161a4bbedf26913d36cc
Hannes Preishuber <hannesp@ppedv.de>
2019-12-03 15:11:55 +0100

    Middleware Image Resize

--- WienCore2019/Pages/Modul05/Thumbnail.cs
+++ WienCore2019/Pages/Modul05/Thumbnail.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,7 +20,14 @@ namespace WienCore2019.Pages.Modul05
             string name = Path.GetFileName(img); //wegen Edge bzw Chrome unterschiede
             string[] str = { AppDomain.CurrentDomain.GetData("pfad").ToString(), "wwwroot", "upload", name };
             var pfad = Path.Combine(str);
+            //  Install-Package System.Drawing.Common
+
+            Image image = Image.FromFile(pfad);
+            Image thumb = image.GetThumbnailImage(300, 200, () => false, IntPtr.Zero);
+           
             await context.Response.SendFileAsync(pfad);
+           
+           // await _next(context);
         }
     }
 }
