f76b63d1714eb71cd1995bfb499a3cc2e89cece9
Hannes Preishuber <hannesp@ppedv.de>
2019-12-03 10:50:28 +0100

    Cookie Setzen und JavaScript lesen

---
+++ WienCore2019/Pages/Modul05/TestCookie.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class TestCookieModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        public void OnGetSetzKeks()
+        {
+            Response.Cookies.Append("temp", "Jammes");
+            var fixCookie = new CookieOptions()
+            {
+                IsEssential = true,
+                Expires = new DateTimeOffset(DateTime.Now.AddDays(30))
+
+            };
+            Response.Cookies.Append("dauerhaft", "Hannes", fixCookie);
+        }
+    }
+}
