8869773f65356148b758f91df36812a41e0d74fb
Hannes Preishuber <hannesp@ppedv.de>
2019-12-03 10:24:00 +0100

    Export Excel

---
+++ WienCore2019/Pages/Modul05/ExportExcel.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class ExportExcelModel : PageModel
+    {
+        public void OnGet()
+        {
+            HttpContext.Response.ContentType = "application/vnd.ms-excel";
+        }
+    }
+}
---
+++ WienCore2019/Pages/Modul05/TestHttp.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class TestHttpModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+            HttpContext.Response.StatusCode = 404;
+        }
+    }
+}
