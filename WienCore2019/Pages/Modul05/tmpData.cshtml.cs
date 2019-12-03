416e34ed7904ae280a79e2fce7eb454e358ab763
Hannes Preishuber <hannesp@ppedv.de>
2019-12-03 12:00:04 +0100

    Tempdata

---
+++ WienCore2019/Pages/Modul05/tmpData.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class tmpDataModel : PageModel
+    {
+        public void OnGet()
+        {
+            var t = TempData["hannes"];
+           // TempData.Peek("hannes");
+        
+        }
+        public void OnPost([FromForm] string wert)
+        {
+            TempData["hannes"] = wert;
+        }
+    }
+}
