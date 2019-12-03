2ccde1cfa194dae9f9ad2a2aa2c46065c1774d48
Hannes Preishuber <hannesp@ppedv.de>
2019-12-03 14:36:25 +0100

    Bilder Gallery

---
+++ WienCore2019/Pages/Modul05/Bilder.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class BilderModel : PageModel
+    {
+        public List<string> Bilder { get; set; } = new List<string>();
+        public void OnGet()
+        {
+            string[] str = { AppDomain.CurrentDomain.GetData("pfad").ToString(), "wwwroot", "upload" };
+            var pfad = Path.Combine(str);
+            foreach (var item in   Directory.GetFiles(pfad, "*.jpg"))
+            {
+                Bilder.Add(Path.GetFileName(item));
+            }
+          
+        }
+    }
+}
