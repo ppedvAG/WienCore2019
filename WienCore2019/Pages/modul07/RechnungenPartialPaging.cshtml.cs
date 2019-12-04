47a09c06dae6d578d403e0aa2f5b220679988a95
Hannes Preishuber <hannesp@ppedv.de>
2019-12-04 10:23:26 +0100

    Rechnung paging mit Ajax und PArtial

---
+++ WienCore2019/Pages/modul07/RechnungenPartialPaging.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WienCore2019
+{
+    public class RechnungenPartialPagingModel : PageModel
+    {
+
+        [BindProperty(SupportsGet = true)]
+        public int SeiteCounter { get; set; }
+        public  List<Rechnung> Liste{ get; set; }
+        public void OnGet()
+        {
+
+        }
+        public PartialViewResult OnGetLadeNext([FromServices] ERPModel2 ef)
+        {
+            Liste=ef.Rechnung.Skip(SeiteCounter * 10).Take(10).ToList();
+            return Partial("_RechnungenPartialPaging",this);
+        }
+    }
+}
